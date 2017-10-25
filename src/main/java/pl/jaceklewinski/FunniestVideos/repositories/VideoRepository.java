ede1ec15b305b61104f1c73348cfcef66def0bea
Jacek Lewiński <lewinski89@wp.pl>
2017-10-25 13:46:45 +0200

    added video repository

---
+++ src/main/java/pl/jaceklewinski/FunniestVideos/repositories/VideoRepository.java
@@ -0,0 +1,12 @@
+package pl.jaceklewinski.FunniestVideos.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.jaceklewinski.FunniestVideos.models.Video;
+
+import java.util.List;
+
+@Repository
+public interface VideoRepository extends CrudRepository<Video, Integer> {
+    List<Video> findAll();
+}
