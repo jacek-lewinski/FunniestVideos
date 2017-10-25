1e0b8c5fca60474cb1018860e8a70ca94c272d46
Jacek Lewiński <lewinski89@wp.pl>
2017-10-25 13:42:18 +0200

    added video model

---
+++ src/main/java/pl/jaceklewinski/FunniestVideos/models/Video.java
@@ -0,0 +1,70 @@
+package pl.jaceklewinski.FunniestVideos.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private String video_src;
+    private int thumbs_up;
+    private int thumbs_down;
+    private Date date;
+
+    public Video () {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVideo_src() {
+        return video_src;
+    }
+
+    public void setVideo_src(String video_src) {
+        this.video_src = video_src;
+    }
+
+    public int getThumbs_up() {
+        return thumbs_up;
+    }
+
+    public void setThumbs_up(int thumbs_up) {
+        this.thumbs_up = thumbs_up;
+    }
+
+    public int getThumbs_down() {
+        return thumbs_down;
+    }
+
+    public void setThumbs_down(int thumbs_down) {
+        this.thumbs_down = thumbs_down;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
