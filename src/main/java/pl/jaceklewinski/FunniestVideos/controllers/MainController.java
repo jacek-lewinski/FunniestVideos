18c9c814ead0c034655512aeeec9708495e7b417
Jacek Lewiński <lewinski89@wp.pl>
2017-10-25 13:53:18 +0200

    added autowired video repository

--- src/main/java/pl/jaceklewinski/FunniestVideos/controllers/MainController.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/controllers/MainController.java
@@ -1,14 +1,21 @@
 package pl.jaceklewinski.FunniestVideos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.jaceklewinski.FunniestVideos.repositories.VideoRepository;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    VideoRepository videoRepository;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("videos", videoRepository.findAll());
         return "index";
     }
 }
