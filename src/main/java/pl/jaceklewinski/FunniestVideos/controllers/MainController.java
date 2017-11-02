ac5fa86a965e84f4b6b24d353627066fd4563db3
Jacek Lewiński <lewinski89@wp.pl>
2017-11-02 17:18:24 +0100

    added logout option in menu if user is logged and remove login option in menu if user is logged

--- src/main/java/pl/jaceklewinski/FunniestVideos/controllers/MainController.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/controllers/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.jaceklewinski.FunniestVideos.models.UserData;
 import pl.jaceklewinski.FunniestVideos.repositories.VideoRepository;
 
 @Controller
@@ -13,9 +14,13 @@ public class MainController {
     @Autowired
     VideoRepository videoRepository;
 
+    @Autowired
+    UserData userData;
+
     @GetMapping("/")
     public String main(Model model) {
         model.addAttribute("videos", videoRepository.findAll());
+        model.addAttribute("loggedUser", userData.isLogged());
         return "index";
     }
 }
