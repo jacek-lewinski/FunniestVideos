cf480248bcbd158f8770f41da9ff5f39ba444926
Jacek Lewiński <lewinski89@wp.pl>
2017-11-02 16:27:08 +0100

    added proxyMode

--- src/main/java/pl/jaceklewinski/FunniestVideos/models/UserData.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/models/UserData.java
@@ -1,11 +1,12 @@
 package pl.jaceklewinski.FunniestVideos.models;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 @Component
-@Scope(value = WebApplicationContext.SCOPE_SESSION)
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserData {
     private boolean isLogged;
     private User user;
