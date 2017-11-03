4c04669cfb4777a5d0f78edba2fafa001fb23463
Jacek Lewiński <lewinski89@wp.pl>
2017-11-03 11:31:06 +0100

    Changed login method on fully login with Spring Security

--- src/main/java/pl/jaceklewinski/FunniestVideos/config/WebSecurityConfig.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/config/WebSecurityConfig.java
@@ -1,21 +1,43 @@
 package pl.jaceklewinski.FunniestVideos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication (AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, active FROM user WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM user_roles WHERE username=?");
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/fonts/**", "/js/**", "/login", "/registration", "/userpanel").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/", "/css/**", "/fonts/**", "/js/**", "/registration").permitAll()
+                .antMatchers("/userpanel").authenticated()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+        http
+                .exceptionHandling().accessDeniedPage("/403");
 
     }
 }
