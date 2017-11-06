33f797186424e495c2c31d7d6984e25237c61fb2
Jacek Lewiński <lewinski89@wp.pl>
2017-11-06 17:34:31 +0100

    Cleaned up code due added login with Spring Security

--- src/main/java/pl/jaceklewinski/FunniestVideos/controllers/SecureController.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/controllers/SecureController.java
@@ -1,17 +1,17 @@
 package pl.jaceklewinski.FunniestVideos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import pl.jaceklewinski.FunniestVideos.models.User;
-import pl.jaceklewinski.FunniestVideos.models.UserData;
 import pl.jaceklewinski.FunniestVideos.models.forms.UserForm;
 import pl.jaceklewinski.FunniestVideos.repositories.UserRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.Optional;
 
 @Controller
 public class SecureController {
@@ -19,39 +19,13 @@ public class SecureController {
     @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    UserData userData;
-
     @GetMapping("/login")
     public String login(Model model) {
-        model.addAttribute("loggedUser", userData.isLogged());
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String postLogin(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
-        Optional<User> user = userRepository.findByUsername(username);
-
-        if (user.isPresent()) {
-            if (password.equals(user.get().getPassword())) {
-                model.addAttribute("isLogged", true);
-                model.addAttribute("loggedInfo", "Zalogowano poprawnie.");
-                userData.setLogged(true);
-                userData.setUser(user.get());
-                return "redirect:/userpanel";
-            }
-            model.addAttribute("isLogged", false);
-            model.addAttribute("loggedInfo", "Błędne hasło!");
-            return "login";
-        }
-        model.addAttribute("isLogged", false);
-        model.addAttribute("loggedInfo", "Użytkownik o takiej nazwie nie istnieje!");
         return "login";
     }
 
     @GetMapping("/registration")
     public String registration(Model model) {
-        model.addAttribute("loggedUser", userData.isLogged());
         model.addAttribute("userForm", new UserForm());
         return "registration";
     }
@@ -84,21 +58,8 @@ public class SecureController {
     }
 
     @GetMapping("/userpanel")
-    public String userpanel(Model model) {
-        model.addAttribute("loggedUser", userData.isLogged());
-        if (userData.isLogged()) {
-            model.addAttribute("username", userData.getUser().getUsername());
-            return "userpanel";
-        }
-        return "403";
-    }
-
-    @GetMapping("/logout")
-    public String logout() {
-        if (userData.isLogged()) {
-            userData.setLogged(false);
-            return "index";
-        }
-        return "index";
+    public String userpanel(Model model, HttpServletRequest request) {
+        model.addAttribute("username", request.getRemoteUser());
+        return "userpanel";
     }
 }
