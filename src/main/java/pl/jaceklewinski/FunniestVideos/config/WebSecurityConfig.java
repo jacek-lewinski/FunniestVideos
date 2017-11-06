ad45f0dcecf83bd850638121ebb6f5e730ddbaf6
Jacek Lewiński <lewinski89@wp.pl>
2017-11-06 22:17:32 +0100

    Added logoutRequestMatcher because spring security automatically disables GET logut when csrf protection is enabled

--- src/main/java/pl/jaceklewinski/FunniestVideos/config/WebSecurityConfig.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -34,7 +35,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/userpanel").authenticated()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/userpanel")
-                .and().logout().permitAll();
+                .and()
+                .logout()
+                    //spring security default enabled csrf protection which disabled /logout link by get method
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .permitAll();
         http
                 .exceptionHandling().accessDeniedPage("/403");
 
