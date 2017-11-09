d96247ece6046e6f9807fe6bdd92e3a877cf1281
Jacek Lewiński <lewinski89@wp.pl>
2017-11-09 14:43:08 +0100

    Create new method which responsibility for change password in database

--- src/main/java/pl/jaceklewinski/FunniestVideos/repositories/UserRepository.java
+++ src/main/java/pl/jaceklewinski/FunniestVideos/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package pl.jaceklewinski.FunniestVideos.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.jaceklewinski.FunniestVideos.models.User;
@@ -9,4 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    @Query(value = "UPDATE user SET password =?1 WHERE username = ?2")
+    int update(String password, String username);
 }
